021c2cde4be1a6098bb76207bb6cd23059450e71
emre0altan <emrealtann77@gmail.com>
2024-02-27 23:05:46 +0000

    simple wall run added

--- Character/Character.gd
+++ Character/Character.gd
@@ -1,10 +1,10 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 @export var blend_weight = 0.1
+@export var debug_node : Node3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -12,6 +12,19 @@ var blend_position : Vector2
 
 func _physics_process(delta):
 	# Add the gravity.
+	var space_state = get_world_3d().direct_space_state
+	# use global coordinates, not local to node
+	var origin = position + Vector3(0,1.6,0)
+	var target = origin + basis.z * 3
+	var query = PhysicsRayQueryParameters3D.create(origin, target)
+	var result = space_state.intersect_ray(query)
+	debug_node.position = Vector3.ZERO
+	var angle = 0
+	if result.has('position'):
+		angle = basis.z.angle_to(result.normal)
+		if angle < 2.7 and angle > 2:
+			debug_node.global_position = result.position
+		
 	var h = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
 	var v = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
 	var boost = Input.get_action_strength("boost")
@@ -22,9 +35,13 @@ func _physics_process(delta):
 	$AnimationTree.set("parameters/Movement/blend_position", blend_position)
 	$AnimationTree.set("parameters/Crouch/blend_position", blend_position)
 	if Input.is_action_pressed("ui_accept"):
-		$AnimationTree.set("parameters/conditions/jump", true)
-		$AnimationTree.set("parameters/Jump/blend_position", blend_position.y)
+		if result.has('position') and angle < 2.7 and angle > 2:
+			$AnimationTree.set("parameters/conditions/wall_run", true)
+		else:
+			$AnimationTree.set("parameters/conditions/jump", true)
+			$AnimationTree.set("parameters/Jump/blend_position", blend_position.y)
 	else:
+		$AnimationTree.set("parameters/conditions/wall_run", false)
 		$AnimationTree.set("parameters/conditions/jump", false)
 	if Input.is_action_pressed("crouch"):
 		$AnimationTree.set("parameters/conditions/crouching", true)
@@ -34,5 +51,5 @@ func _physics_process(delta):
 		$AnimationTree.set("parameters/conditions/not_crouching", true)
 	velocity = $AnimationTree.get_root_motion_position() / delta
 	var rotatedVel  = -velocity.rotated(Vector3.UP, h_rot)
-	velocity = Vector3(rotatedVel.x, velocity.y - gravity*delta, rotatedVel.z)
+	velocity = Vector3(rotatedVel.x, velocity.y, rotatedVel.z)
 	move_and_slide()
